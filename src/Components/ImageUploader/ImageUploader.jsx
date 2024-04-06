775e5a82364c08f9a9b6fc61fc0130f9893dca1e
Sindhupandi54 <psindhureddy03@gmail.com>
2024-04-06 06:19:47 +0530

    first commit

--- src/Components/ImageUploader/ImageUploader.jsx
+++ src/Components/ImageUploader/ImageUploader.jsx
@@ -1,42 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
-export default function ImageUploader() {
-    const [selectedFiles, setSelectedFiles] = useState(null);
+import ImageUploading from 'react-images-uploading';
+import "./ImageUploader.css";
 
-  const handleFileChange = (event) => {
-    setSelectedFiles(event.target.files);
+export default function ImageUploader() {
+    const service_id =1;
+  const [images, setImages] = useState([]);
+  const maxNumber = 12;
+
+  const onChange = (imageList, addUpdateIndex) => {
+    setImages(imageList);
   };
 
-  const handleUpload = async () => {
-    if (!selectedFiles || selectedFiles.length === 0) {
-      alert('Please select at least one image to upload.');
-      return;
-    }
-
-    const formData = new FormData();
-    for (let i = 0; i < selectedFiles.length; i++) {
-      formData.append('photos', selectedFiles[i]);
-    }
-
+  const handleUpdate = async () => {
+    let formData={service_id:service_id,image:''};
     try {
-      const response = await axios.post('http://localhost:8081/uploadImage', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-
-      console.log(response.data);
+      for (let i = 0; i < images.length; i++) {
+        formData.image=  images[i].file;
+  
+        await uploadImage(formData);
+      }
       alert('Images uploaded successfully!');
-      setSelectedFiles(null);
+      setImages([]);
     } catch (error) {
       console.error('Error uploading images:', error);
       alert('An error occurred while uploading the images. Please try again later.');
     }
   };
-    return (
-        <div>
-            <input type="file" multiple onChange={handleFileChange} />
-            <button onClick={handleUpload}>Upload</button>
-        </div>
-    )
+  
+  const uploadImage = async (formData) => {
+    try {
+      await axios.post('http://localhost:8081/uploadImage', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+    } catch (error) {
+      throw new Error('Error uploading image:', error);
+    }
+  };
+  
+  
+
+  return (
+    <div>
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          <div className="upload__image-wrapper">
+            <div className="Upload-buttons-wrapper">
+              <button
+                className="Upload-buttons"
+                style={isDragging ? { color: 'red' } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Click or Drop here
+              </button>
+              &nbsp;
+              <button onClick={onImageRemoveAll} className="Upload-buttons">
+                Remove all images
+              </button>
+              {images.length > 0 && (
+                <button onClick={handleUpdate} className="Upload-buttons">
+                  Upload Images
+                </button>
+              )}
+            </div>
+            <div className="selected-images">
+              {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                  <img src={image['data_url']} alt="" width="100" name='image'/>
+                  <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageUpdate(index)} className="handle-buttons">
+                      Update
+                    </button>
+                    <button onClick={() => onImageRemove(index)} className="handle-buttons">
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </ImageUploading>
+    </div>
+  );
 }
