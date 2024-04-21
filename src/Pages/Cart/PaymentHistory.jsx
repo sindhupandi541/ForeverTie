7f2f3485278cbe00fd53b8c4d64eba44dafca67d
sindhupandi541 <167642883+sindhupandi541@users.noreply.github.com>
2024-04-21 16:55:25 +0530

    Update PaymentHistory.jsx

--- src/Pages/Cart/PaymentHistory.jsx
+++ src/Pages/Cart/PaymentHistory.jsx
@@ -17,7 +17,7 @@ export default function PaymentHistory() {
     const fetchPaymentHistory = async () => {
       try {
         const userId = window.sessionStorage.getItem('UserId');
-        const response = await axios.post('https://server-tjm9.onrender.com', { userId });
+        const response = await axios.post('https://server-tjm9.onrender.com/paymentHistory', { userId });
         console.log(response.data.paymentHistory);
         setPaymentHistory(response.data.paymentHistory);
         setLoading(false);
